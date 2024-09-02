5ea7fa8dd93b4692e353257817dc492c10dba3cd
Your Name <you@example.com>
2024-09-02 14:03:49 +0530

    spread operator

---
+++ New/css/javaScript/spread operator/home.js
@@ -0,0 +1,9 @@
+// let user='hello'
+// let name='hello'
+// export default name
+function sum(a,b){
+console.log(a+b);
+
+}
+let user='hello'
+export{user,sum}
---
+++ New/css/javaScript/spread operator/spred.js
@@ -0,0 +1,52 @@
+// let arr=[1,2,3,4,5,6]
+// let num=[4,5,6,8,9,34,...arr]
+// let obj={
+// id:2,
+// lastName:'hello'
+// }
+// let std={
+// id:1,
+// name:'hehheee',
+// ...obj
+// }
+// console.log( num,'hehheee');
+
+
+// another  way(rest parameter)
+// function sum(a,b,...num){
+
+// // console.log(a+b);
+// console.log(num,"hehe");
+
+// }
+
+// sum(5,5,6,78,3,56,77,23)
+// let a=5
+// let arr=[hehe]+5
+
+// D structuring
+// let arr=[1,2,3,5,6,7,8]
+// let[a,b,c,d,f,e]=arr
+// console.log(a);
+
+// by object D STRUCUTING
+
+//  let obj={
+// id:1,
+// name:'hey'
+//  }
+// let {
+// id
+// }=obj
+// console.log(id);
+
+// export and inport
+// import a from './home.js'
+// console.log(a,"hehe");
+import {sum ,user} from './home.js'
+// console.log(sum(5,5),user,"hehe");
+
+sum(6,5)
+
+
+
