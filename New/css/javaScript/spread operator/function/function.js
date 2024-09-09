cc9d1677b5db75cae8fbca3b7cdac0fc6c6ff0b4
Your Name <you@example.com>
2024-09-09 14:29:59 +0530

    new

---
+++ New/css/javaScript/spread operator/function/function.js
@@ -0,0 +1,31 @@
+// let str="HelloWorld";
+// console.log(str.indexOf("World"));   **output of index ex -3
+// console.log(str.indexOf("H"));
+// let str="Hello,World";
+// console.log(str.includes("World"));  *** output true / false
+// let str="Hello, World";
+// console.log(str.slice(0,5));         ***output "index value name ex- hello"
+// console.log(str.slice(7));
+// let str="Hello, World";
+// console.log(str.replace("World","Universe"));***it replace the value 
+
+// ***Prototype***
+// let obj={
+// id:1,
+// name:"hello"
+// }
+// console.log(obj.toString());
+
+// let arr=[1,2,3,4,5]
+
+// let str="hello"
+// localStorage.setItem('data','hellooo')
+// let a=localStorage.getItem('data','hellooo')
+// console.log(a);
+
+
+// creat a form and store all the data in localStprage
+// let btn=  document.querySelector('button')
+// let inp=  document.querySelector('input')
+// localStorage.setItem('data','hello')
+// let a=localStorage.getItem('data','hello')
